ea148f8b20ce5527e3c56ca839e1358cefac8c8a
Nico Rithner <nicorithner@gmail.com>
2022-01-25 21:31:27 -0700

    Initial commit. Some lessons and start Pong game

---
+++ _main.lua
@@ -0,0 +1,51 @@
+-- function love.draw()
+--     love.graphics.print("Hello World", 400, 300)
+-- end
+
+-- result = 10 - 5
+--
+-- function love.draw()
+--   love.graphics.print(result)
+-- end
+
+-- -- Tables
+-- OurAwesomeTable = {}
+-- OurAwesomeTable.coolVariable = 10
+-- OurAwesomeTable.coolTable = {}
+--
+-- AnotherTable = {newVariable = 10, newTable = {}}
+--
+-- print(AnotherTable.newVariable)
+
+-- operators
+-- ~= not equal
+-- == equla
+-- > greater than
+-- < less than
+
+-- -- function
+--
+-- money = 290
+--
+-- function checkWealth()
+--   if money > 100 and money < 200 then
+--     print(money)
+--   elseif money <= 100 then
+--     print('Not enough money')
+--   else
+--     print("I'm rich")
+--   end
+-- end
+--
+-- checkWealth()
+
+-- -- LOVE functions
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
---
+++ games/pong/player.lua
@@ -0,0 +1,16 @@
+Player = {}
+
+function Player:load()
+  self.x = 50
+  self.y = love.graphics.getHeight() / 2
+  self.width = 20
+  self.height = 100
+  self.speed = 500
+end
+
+function Player:update(dt)
+end
+
+function Player:draw()
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
