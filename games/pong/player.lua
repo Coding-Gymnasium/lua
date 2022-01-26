93f41e85970d5e19d9d709344373a7f9c577582f
Nico Rithner <nicorithner@gmail.com>
2022-01-25 21:43:12 -0700

    Add checkboundaries and move functions

--- games/pong/player.lua
+++ games/pong/player.lua
@@ -9,6 +9,24 @@ function Player:load()
 end
 
 function Player:update(dt)
+  self:move(dt)
+  self:checkBoundaries()
+end
+
+function Player:move(dt)
+  if love.keyboard.isDown('w') then
+    self.y = self.y - self.speed * dt
+  elseif love.keyboard.isDown('s') then
+    self.y = self.y + self.speed * dt
+  end
+end
+
+function Player:checkBoundaries()
+  if self.y < 0 then
+    self.y = 0
+  elseif self.y + self.height > love.graphics.getHeight() then
+    self.y = love.graphics.getHeight() - self.height
+  end
 end
 
 function Player:draw()
